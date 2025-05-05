2724777463c4834858ef380f18f76b135a24f0d7
Courtesi <c.chang673@gmail.com>
2025-05-04 22:59:50 -0700

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+sudo mv ../application-prod.properties src/main/resources/
 sudo chmod +x mvnw
 sudo mvn clean install
 sudo mkdir -p target/dependency && (cd target/dependency; sudo jar -xf ../*.jar)
-sudo docker build -t springio/wenslo-page .
-sudo ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/wenslo-page
-sudo docker run -p 8080:8080 -t springio/wenslo-page
+sudo docker build -t wenslo-page .
+sudo ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=wenslo-page
+sudo docker run -p 8080:8080 -d wenslo-page
