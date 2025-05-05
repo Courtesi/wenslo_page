9c1765ffcb5e1908b0e31e5d85c50c15779b461c
Courtesi <c.chang673@gmail.com>
2025-05-05 05:32:53 -0700

    removed duplicated build

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ sudo chmod +x mvnw
 sudo mvn clean install
 sudo mkdir -p target/dependency && (cd target/dependency; sudo jar -xf ../*.jar)
 sudo docker build -t wenslo-page .
-sudo ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=wenslo-page
+#sudo ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=wenslo-page
 sudo docker run -p 8080:8080 -d wenslo-page
